6fdec992e018c1f32b78008f37e699b293a01291
Aalind Mukati <aalindmukati@gmail.com>
2026-06-23 17:29:46 +0530

    Graph Plot test yeah

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ df_sorted = df.sort_values(by='HoursPlayed',ascending=False).reset_index(drop=Tr
 print(df_sorted)
 
 # Plotting graph test
+
+
+ax1.plot(df['Name'],df['HoursPlayed'],color='red',marker='o',linewidth=2.5)
+ax1.set_xlabel('Name',fontsize=12)
+ax1.set_ylabel('HoursPlayed',fontsize=12)
+ax1.tick_param(axis='x',color='blue')
+ax1.grid(True,linestyle=':',alpha=0.5)
+
+mat.title('Test Graph')
+mat.show()
