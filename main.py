039b9d1afef635b6a3cd66e1099d4da14c1a269a
Aalind Mukati <aalindmukati@gmail.com>
2026-06-23 17:19:36 +0530

    Graph Plot test

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import seaborn as sns
 import matplotlib as mat
 
 df = pd.read_csv('Data.csv')
-df_sorted = df.sort_values(by='HoursPlayed',ascending=False)
+df_sorted = df.sort_values(by='HoursPlayed',ascending=False).reset_index(drop=True) 
 print(df_sorted)
+
+# Plotting graph test
