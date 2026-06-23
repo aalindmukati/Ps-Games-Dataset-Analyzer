a468fbd0e8f114cb7456d274c2fa13574a2ca3fb
Aalind Mukati <aalindmukati@gmail.com>
2026-06-23 17:41:31 +0530

    Figure 1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import seaborn as sns
-import matplotlib as mat
+import matplotlib.pyplot as mat
 
 df = pd.read_csv('Data.csv')
 df_sorted = df.sort_values(by='HoursPlayed',ascending=False).reset_index(drop=True) 
@@ -8,12 +8,15 @@ print(df_sorted)
 
 # Plotting graph test
 
+fig,ax1 = mat.subplots(figsize =(15,6))
+
 
 ax1.plot(df['Name'],df['HoursPlayed'],color='red',marker='o',linewidth=2.5)
-ax1.set_xlabel('Name',fontsize=12)
+ax1.set_xlabel('Game Name',fontsize=12)
 ax1.set_ylabel('HoursPlayed',fontsize=12)
-ax1.tick_param(axis='x',color='blue')
+ax1.tick_params(axis='x',color='blue')
 ax1.grid(True,linestyle=':',alpha=0.5)
 
 mat.title('Test Graph')
 mat.show()
+
